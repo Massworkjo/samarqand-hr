90743df02234eb428e6cfc63edd582a603de769d
Massworkjo <info@samarkandsweets.com>
2026-07-20 18:37:46 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* سمرقند HR — Service Worker */
-const CACHE = 'smq-hr-v1';
+const CACHE = 'smq-hr-v2';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting()));
